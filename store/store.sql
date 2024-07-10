02d9754d6b751d67036682aa0ad5c30494f6e28a
Tim Ludwiczak <tim.ludwiczak@gmail.com>
2024-07-10 07:04:51 -0500

    Updates to store.sql

--- store/store.sql
+++ store/store.sql
@@ -1,69 +1,4 @@
--- Drop the table if it exists
-DROP TABLE IF EXISTS game;
-
-DROP TABLE IF EXISTS genre;
-CREATE TABLE genre (
-    genre_id SERIAL PRIMARY KEY,
-    genre_name VARCHAR(255) UNIQUE NOT NULL CHECK (genre_name ~ )
-);
-
--- Create Game Table
-CREATE TABLE game (
-    game_id INT PRIMARY KEY,
-    game_title VARCHAR(255) UNIQUE NOT NULL CHECK (game_title ~ '^[A-Za-z0-9 _\-:''\\]+$'),
-    quantity INT NOT NULL CHECK(quantity > 0 AND quantity < 51),
-    price DECIMAL(5, 2) NOT NULL CHECK(price > 10 AND price < 60)
-);
-
--- Insert Sample Data into Game Table
-\COPY game FROM './data/game.csv' WITH CSV HEADER;
-
-
-DROP TABLE IF EXISTS action_figure;
-
-CREATE TABLE action_figure (
-    id INT PRIMARY KEY,
-    action_figure_title VARCHAR UNIQUE NOT NULL CHECK (action_figure_title ~ '^[A-Za-z0-9 _-]+$'),
-    quantity INT NOT NULL CHECK(quantity > 0 AND quantity < 31),
-    price DECIMAL(5,2) NOT NULL CHECK(price > 10 AND price < 100.01)
-);
-
-\COPY action_figure FROM './data/action_figure.csv' WITH CSV HEADER;
-
-DROP TABLE IF EXISTS employee;
-
--- MAX SALARY AN HOUR IS 31.25 AND MIN IS 16.66
-CREATE TABLE employee (
-    id INT PRIMARY KEY,
-    employee_name VARCHAR NOT NULL CHECK (employee_name ~ '^[A-Za-z ]+$'),
-    position VARCHAR NOT NULL CHECK (position IN (
-        'Sales Associate',
-        'Store Manager',
-        'Inventory Clerk',
-        'Customer Service Representative',
-        'IT Specialist',
-        'Marketing Coordinator',
-        'Assistant Manager',
-        'Finance Analyst',
-        'Security Officer',
-        'HR Coordinator'
-    )),
-    salary DECIMAL(7,2) NOT NULL CHECK (salary > 31987.19 AND salary < 60000.01)
-);
-
-\COPY employee FROM './data/employee.csv' WITH CSV HEADER;
-
-DROP TABLE IF EXISTS poster;
-
-CREATE TABLE poster (
-    id SERIAL PRIMARY KEY,
-    poster_title VARCHAR UNIQUE NOT NULL CHECK (poster_title ~ '^[A-Za-z0-9 _-]+$'),
-    quantity INT NOT NULL CHECK (quantity > 0 AND quantity < 31),
-    price DECIMAL(4,2) NOT NULL CHECK (price <= 20.00 AND price > 6)
-);
-
-
-\COPY poster FROM './data/poster.csv' WITH CSV HEADER;
+-- \COPY poster FROM './data/poster.csv' WITH CSV HEADER;
 
 -- Drop the table if it exists
 DROP TABLE IF EXISTS game CASCADE;
@@ -82,6 +17,13 @@ CREATE TABLE game (
 -- Insert Sample Data into Game Table
 \COPY game FROM './data/game.csv' WITH CSV HEADER;
 
+DROP TABLE IF EXISTS genres CASCADE;
+
+CREATE TABLE genres (
+    genre_id SERIAL PRIMARY KEY,
+    genre VARCHAR(255) NOT NULL UNIQUE
+)
+
 DROP TABLE IF EXISTS genre_game CASCADE;
 
 CREATE TABLE genre_game (
@@ -91,10 +33,21 @@ CREATE TABLE genre_game (
     FOREIGN KEY (game_id) REFERENCES game (game_id),
     FOREIGN KEY (genre_id) REFERENCES genre (genre_id)
 );
+DROP TABLE IF EXISTS gaming_engines;
+
+CREATE TABLE gaming_engines (
+    engine_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE
+);
+
+ALTER TABLE games
+ADD COLUMN engine_id INT,
+ADD FOREIGN KEY (engine_id) REFERENCES gaming_engines(engine_id);
+
 
 \COPY genre_game FROM './data/genre_game.csv' WITH CSV HEADER;
 
-DROP TABLE IF EXISTS action_figure;
+DROP TABLE IF EXISTS action_figure CASCADE;
 
 CREATE TABLE action_figure (
     id SERIAL PRIMARY KEY,
@@ -105,7 +58,7 @@ CREATE TABLE action_figure (
 
 \COPY action_figure FROM './data/action_figure.csv' WITH CSV HEADER;
 
-DROP TABLE IF EXISTS poster;
+DROP TABLE IF EXISTS poster CASCADE;
 
 CREATE TABLE poster (
     id SERIAL PRIMARY KEY,
@@ -139,7 +92,7 @@ CREATE TABLE employee (
 
 \COPY employee FROM './data/employee.csv' WITH CSV HEADER;
 
-DROP TABLE IF EXISTS social_security;
+DROP TABLE IF EXISTS social_security CASCADE;
 
 CREATE TABLE social_security(
     id SERIAL PRIMARY KEY,
@@ -150,7 +103,7 @@ CREATE TABLE social_security(
 
 \COPY social_security FROM './data/social_security.csv' WITH CSV HEADER;
 
-DROP TABLE IF EXISTS shifts;
+DROP TABLE IF EXISTS shifts CASCADE;
 
 CREATE TABLE shifts (
     id SERIAL PRIMARY KEY,
